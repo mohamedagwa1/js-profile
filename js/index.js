856dd8f4ef2704dc557fe795a3fd5abd15851b6a
Mohamed agwa <116589079+mohamedagwa1@users.noreply.github.com>
2023-04-24 01:02:36 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const sections =document.querySelectorAll('.section');
+const secBtns =document.querySelectorAll('.controlls');
+const secBtn =document.querySelectorAll('.control');
+const allSections =document.querySelector('.main-content');
+/////////////////////////
+function PageTransitions(){
+    //button click active class
+    for(let i=0; i<secBtn.length ;i++){
+        secBtn[i].addEventListener('click',function(){
+            let currentItem =document.querySelectorAll('.active-btn');
+            currentItem[0].className=currentItem[0].className.replace('active-btn','');
+            secBtn[i].className+=' active-btn';
+        })
+    }
+    //section active class
+    allSections.addEventListener('click',function(e){
+        let id =e.target.dataset.id ;
+        if(id){
+            //remove selected from other btns
+            secBtn.forEach((btn)=>{
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active');
+            })
+            const element= document.getElementById(id);
+            element.classList.add('active');
+        }
+    }
+    );
+}
+PageTransitions()
